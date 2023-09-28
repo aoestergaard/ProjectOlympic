5e753b76dbe21007ae22b5d338a4cdd4ff772fae
aoestergaard <121874444+aoestergaard@users.noreply.github.com>
2023-09-28 13:49:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ document.getElementById("dashboard").addEventListener("click", function () {
 function loadDashboardContent() {
     const dashboardContent = document.getElementById("dashboard");
     dashboardContent.innerHTML = `
-        <!-- Your Dashboard content here -->
+      function loadDashboardContent() {
+    const dashboardContent = document.getElementById("dashboard");
+    dashboardContent.innerHTML = `
+        <h2>Dashboard</h2>
+        <div class="dashboard-graph">
+            <!-- Example graph here -->
+            <img src="example_graph.png" alt="Returns Over 5 Years">
+        </div>
+        <div class="dashboard-table">
+            <table>
+                <tr>
+                    <th># of Companies Analyzed</th>
+                    <th># of Companies Bought</th>
+                    <th>Current Fund Multiple</th>
+                </tr>
+                <tr>
+                    <td>50</td>
+                    <td>15</td>
+                    <td>2.5x</td>
+                </tr>
+            </table>
+        </div>
+    `;
+}
     `;
 }
 
