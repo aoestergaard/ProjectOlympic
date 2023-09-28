a3da5d59563b329305089ddfabc6b5cbfd8c9515
aoestergaard <121874444+aoestergaard@users.noreply.github.com>
2023-09-28 13:53:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,33 +28,3 @@ function loadDashboardContent() {
     `;
 }
 
-
-
-document.getElementById("company-tab").addEventListener("click", function () {
-    loadCompanyUniverseContent();
-});
-
-document.getElementById("progress-tab").addEventListener("click", function () {
-    loadProgressTrackerContent();
-});
-
-document.getElementById("resources-tab").addEventListener("click", function () {
-    loadResourcesContent();
-});
-
-// Functions to load content for each tab
-function loadDashboardContent() {
-    // Load dashboard content here
-}
-
-function loadCompanyUniverseContent() {
-    // Load company universe content here
-}
-
-function loadProgressTrackerContent() {
-    // Load progress tracker content here
-}
-
-function loadResourcesContent() {
-    // Load resources content here
-}
